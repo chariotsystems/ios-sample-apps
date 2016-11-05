1616b09697193e3c7b2852ce3ab69629daef8f51
alex eadie <alex.eadie@team.telstra.com>
2016-11-05 22:03:26 +1100

    NetworkUtils finally tidy!

--- BasicPlaybackSampleApp/BasicPlaybackSampleApp/NetworkUtils.swift
+++ BasicPlaybackSampleApp/BasicPlaybackSampleApp/NetworkUtils.swift
@@ -15,20 +15,9 @@ import NetworkExtension
         let url = NSURL(string: urlString)
         let request = URLRequest(url: url! as URL)
         
-        
-/*        let passwordString = "mobile:oooSecret"
-        let passwordData = passwordString.data(using: String.Encoding.utf8)
-        let base64EncodedCredential = passwordData!.base64EncodedString(options: NSData.Base64EncodingOptions())
-        let auth = "Basic \(base64EncodedCredential)" 
-        request.setValue(_:auth, forHTTPHeaderField:"Authorization")
-         */
-        
-       
         if let operation = AFHTTPRequestOperation(request: request) {
-            // Comment out the next line if json deserialize is not required
             operation.responseSerializer = AFJSONResponseSerializer()
-            let credential = URLCredential(user:"mobile",password:"oooSecret",persistence: URLCredential.Persistence.none)
-            operation.credential = credential
+            operation.credential = URLCredential(user:"mobile",password:"oooSecret",persistence: URLCredential.Persistence.none)
             return operation;
         }
         return nil;
